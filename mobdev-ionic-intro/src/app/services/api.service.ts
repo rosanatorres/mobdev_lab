fde0af652243ae23d64df5daf117944126924cde
rosanatorres <rosanacardoso297@hotmail.com>
2022-10-07 15:16:42 +0000

    Created Api Service

---
+++ mobdev-ionic-intro/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  getFilms(){
+    return this.http.get('https://swapi.dev/api/films');
+  }
+
+  getFilm(id){
+    return this.http.get('https://swapi.dev/api/film/${id}');
+  }
+}
